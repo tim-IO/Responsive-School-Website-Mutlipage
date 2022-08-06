1d237ba85445721610056825261c0e4b16130369
tim.eth <timIO.eth@gmail.com>
2022-08-06 15:52:52 +0100

    main

--- App.js
+++ App.js
@@ -4,3 +4,21 @@ window.addEventListener('scroll', () => {
     document.quarySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 0)
 })
+
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => (
+    faq.addEventListener('click', () => (
+        faq.classList.toggle('open')
+
+        // const icon = faq.quarySelector('.faq-icon i');
+        // if(icon.className === 'fa-plus') {
+        //     icon.className = 'fa-minus';
+        // } else {
+        //     icon.className = 'fa-plus';
+        // }
+    ))
+))
